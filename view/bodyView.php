0e0781d32026f64a110760f02e3fc075847a9c51
Besart Elezi <besart.elezi@hotmail.com>
2022-07-01 10:30:20 +0200

    added view

---
+++ view/bodyView.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+?>
+
+<body>
+<form action ="bodyView.php" method="post">
+    Title of your post: <input type="text" name="title"><br>
+    What would you like to post on this website?: <input type="text" name="content"><br>
+    Name: <input type="text" name="content"><br>
+    <input type="submit">
+</form>
+</body>
---
+++ view/headerView.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BlackJack with JackBlack</title>
+</head>
+
