b0c87f4cc2f4c0c13d1afb2095ff80bb15f69515
Besart Elezi <besart.elezi@hotmail.com>
2022-07-01 10:30:05 +0200

    added index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+require 'classes/Post.php';
+require 'classes/PostLoader.php';
+require 'view/headerView.php';
+require 'view/bodyView.php';
+require 'view/footerView.php';
+
+?>
