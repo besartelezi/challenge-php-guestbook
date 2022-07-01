368d4253e8bcc9110137a3eb242e901dc5ed2cdc
Besart Elezi <besart.elezi@hotmail.com>
2022-07-01 12:04:40 +0200

    changed header title

--- view/headerView.php
+++ view/headerView.php
@@ -8,6 +8,6 @@ declare(strict_types=1);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BlackJack with JackBlack</title>
+    <title>Besart's Big Book of Guests</title>
 </head>
 
