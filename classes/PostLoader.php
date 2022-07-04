1599461c858df40ba1cb6f4a9b6edbc615018184
Besart Elezi <besart.elezi@hotmail.com>
2022-07-04 11:00:35 +0200

    added savePost and getPost functions

--- classes/PostLoader.php
+++ classes/PostLoader.php
@@ -3,15 +3,24 @@ declare(strict_types=1);
 
 class PostLoader
 {
-    private string $userInput;
-
-    public function __construct(string $userInput)
-    {
-        $this->userInput = $userInput;
+    public  function savePost () {
+        $userInput = new Post($_POST['title'], $_POST['content'], $_POST['authorName'], $_POST['date']);
+        $guestbook = 'guestbook.txt';
+        $userInputArray = array("title" => $userInput->getTitle(),
+            "content" => $userInput->getContent(),
+            "authorName" => $userInput->getAuthorName(),
+            "date" => $userInput->getDate());
+        $currentPost = file_get_contents($guestbook);
+        $decodeGuestbook = json_decode($currentPost);
+        $decodeGuestbook[] = $userInputArray;
+        $encodeGuestbook = json_encode($decodeGuestbook);
+        file_put_contents($guestbook, $encodeGuestbook);
     }
 
-    public function arrayToString () {
-        $this->userInput = json_encode($_POST);
+    public function  getPosts () {
+        $guestbook = 'guestbook.txt';
+        file_get_contents($guestbook, true);
+        $guestbookPosts = file_get_contents($guestbook, true);
+        return json_decode($guestbookPosts, true);
     }
-
 }
