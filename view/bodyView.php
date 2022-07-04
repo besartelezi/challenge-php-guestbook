8b89187c5d344defd33b2a48398b40d4953323d9
Besart Elezi <besart.elezi@hotmail.com>
2022-07-04 11:01:32 +0200

    added code so user can view posts

--- view/bodyView.php
+++ view/bodyView.php
@@ -1,5 +1,12 @@
 <?php
 declare(strict_types=1);
+
+$postLoader = new PostLoader();
+if (isset($_POST['date']))
+{
+    $postLoader->savePost();
+}
+
 ?>
 
 <body>
@@ -18,6 +25,42 @@ declare(strict_types=1);
         Name:
         <input type="text" name="authorName">
     </label><br>
-    <button type="submit" value="<?php echo (date('m/d/Y')); ?>" name="date">Submit</button>
+    <button type="submit" value="<?php echo (date('d/m/Y')); ?>" name="date">Submit</button>
 </form>
 </body>
+
+
+<?php
+
+$postLoader = new PostLoader();
+
+if($postLoader->getPosts()){
+    $postsArray = $postLoader->getPosts();
+}else{
+    $postsArray = [];
+}
+
+$i = 0;
+
+$textEmojis = [':)', ':-)', ':(', ':-(', ':D', ':-D', 'xD', ':o', ':-o'];
+$emojis = ['&#128522', '&#128522', '&#128543', '&#128543', '&#128516', '&#128516', '&#128518', '&#128558', '&#128558'];
+
+$nonoWords = ['fuck', 'fucking', 'fuckyou', 'shit','hell','damn','asshole','pussy','dipshit','fucker'];
+$yesyesWords = ['heck','hecking','i love you','jelly-o','o heavens','darn','good kid','brave soldier','dipper','hero'];
+?>
+<?php foreach(array_reverse($postsArray) as $value): ?>
+    <?php $i++;
+    $content = $value['content'];
+    $filteredContent = str_replace($nonoWords, $yesyesWords, $content);
+    str_replace($textEmojis, $emojis ,$filteredContent);
+    ?>
+    <div class="card" style="background-color: lightblue; margin: 15px; padding 30px;">
+        <div class="image"><img src="https://picsum.photos/70" alt="random image">
+            <div class="Title"><?= $value['title'] ?></div>
+            <div class="authorName"><?= $value['authorName'] ?></div>
+            <div> <?= str_replace($textEmojis, $emojis ,$filteredContent); ?></div>
+            <h6> <?= $value['date'] ?></h6>
+        </div>
+    </div>
+    <?php if ($i >= 20) break;
+endforeach; ?>
